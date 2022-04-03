3c5c3af317ca05b72d0c304dbdde2bfebb891f22
baconsplit <tristan.stoll@gmail.com>
2022-04-03 13:36:54 +0200

    -#app +app

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ using Bcnfolio;
 using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<App>("app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
